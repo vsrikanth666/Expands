bada70b03d83efab001dd95bf0a79a87ec2b2ceb
vsrikanth666 <39478556+vsrikanth666@users.noreply.github.com>
2018-05-22 17:54:00 +0530

    Add files via upload

---
+++ IfCondition.java
@@ -0,0 +1,23 @@
+
+public class IfCondition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x = 10;
+		int y = 10;
+		if(x < y){
+			System.out.println("a is less than b");
+		}else if(x > y){
+			System.out.println("a is greater than b ");
+		}else{
+			System.out.println("a is equal to b");
+		}
+		
+		
+		
+		
+		
+	}
+
+}
